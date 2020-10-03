df0555fd2f25d6bec8f9fa9e5c68e54e951527ad
aIjundi <sohaibon@gmail.com>
2020-10-03 14:09:22 +0300

    initial commit

---
+++ LOSMultiRunner/MainForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace LOSMultiRunner
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LOSDir = new System.Windows.Forms.TextBox();
+            this.Browse = new System.Windows.Forms.Button();
+            this.Run = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.CleanUp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LOSDir
+            // 
+            this.LOSDir.Enabled = false;
+            this.LOSDir.Location = new System.Drawing.Point(12, 12);
+            this.LOSDir.Name = "LOSDir";
+            this.LOSDir.Size = new System.Drawing.Size(420, 20);
+            this.LOSDir.TabIndex = 0;
+            this.LOSDir.Text = (string)Properties.Settings.Default["LOSDir"];
+            // 
+            // Browse
+            // 
+            this.Browse.Location = new System.Drawing.Point(12, 38);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(100, 23);
+            this.Browse.TabIndex = 1;
+            this.Browse.Text = "Browse";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // Run
+            // 
+            this.Run.Location = new System.Drawing.Point(119, 38);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(100, 23);
+            this.Run.TabIndex = 2;
+            this.Run.Text = "Run";
+            this.Run.UseVisualStyleBackColor = true;
+            this.Run.Click += new System.EventHandler(this.Run_Click);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(226, 38);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(100, 23);
+            this.Save.TabIndex = 3;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // CleanUp
+            // 
+            this.CleanUp.Location = new System.Drawing.Point(333, 38);
+            this.CleanUp.Name = "CleanUp";
+            this.CleanUp.Size = new System.Drawing.Size(100, 23);
+            this.CleanUp.TabIndex = 4;
+            this.CleanUp.Text = "CleanUp";
+            this.CleanUp.UseVisualStyleBackColor = true;
+            this.CleanUp.Click += new System.EventHandler(this.CleanUp_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 70);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Run);
+            this.Controls.Add(this.CleanUp);
+            this.Controls.Add(this.Browse);
+            this.Controls.Add(this.LOSDir);
+            this.Name = "MainForm";
+            this.Text = "LOSMultiRunner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LOSDir;
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.Button Run;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button CleanUp;
+    }
+}
+
---
+++ LOSMultiRunner/MainForm.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace LOSMultiRunner
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.ShowNewFolderButton = false;
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (File.Exists(folderBrowserDialog.SelectedPath + "\\LOS.exe"))
+                    LOSDir.Text = folderBrowserDialog.SelectedPath;
+                else
+                    MessageBox.Show("Choose a correct game directory");
+            }
+        }
+
+        private void Run_Click(object sender, EventArgs e)
+        {
+            if(!File.Exists(LOSDir.Text + "\\LOS.exe"))
+            {
+                MessageBox.Show("Choose a correct game directory");
+            }
+            else
+            {
+                byte[] LOSexe = File.ReadAllBytes(LOSDir.Text + "\\LOS.exe");
+                string mutex = "program404";
+
+                int index = SearchBytes(LOSexe, System.Text.Encoding.Default.GetBytes(mutex));
+
+                for (int i = 0; i < mutex.Length; i++)
+                    LOSexe[index + i] = 0;
+
+                for (int instancesCount = 0; ; instancesCount++)
+                {
+                    try
+                    {
+                        File.WriteAllBytes(LOSDir.Text + "\\LOS" + instancesCount + ".exe", LOSexe);
+                        ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd", "/min /C \"set __COMPAT_LAYER=RUNASINVOKER && " + LOSDir.Text.Split(':')[0] + ": && cd " + LOSDir.Text + " && " + "start LOS" + instancesCount + ".exe CDLDKJFRE342LDLSL39\"");
+                        processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                        Process.Start(processStartInfo);
+                        break;
+                    }
+                    catch { }
+                }
+
+                Application.Exit();
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default["LOSDir"] = LOSDir.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void CleanUp_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(LOSDir.Text + "\\LOS.exe"))
+            {
+                MessageBox.Show("Choose a correct game directory");
+            }
+            else
+            {
+                string[] filesToDelete = Directory.GetFiles(LOSDir.Text, "LOS*.exe");
+
+                for (int i = 0; i < filesToDelete.Length; i++)
+                {
+                    try
+                    {
+                        File.Delete(LOSDir.Text + "\\LOS" + i + ".exe");
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Please close all game instances to run correctly");
+                    }
+                }
+            }
+        }
+
+        private int SearchBytes(byte[] arr, byte[] subarr)
+        {
+            for (var i = 0; i <= arr.Length - subarr.Length; i++)
+            {
+                var k = 0;
+
+                for (; k < subarr.Length; k++)
+                    if (subarr[k] != arr[i + k])
+                        break;
+                
+                if (k == subarr.Length)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
